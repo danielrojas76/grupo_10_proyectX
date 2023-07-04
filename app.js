bb06a9b4e0040cc8b7def3f7e7d06910868cd07e
maticevallos <matiascevallos7@gmail.com>
2023-07-03 21:03:27 -0300

    estructura de archivos creada

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const app = express();
+const path =  require("path");
+
+app.use(express.static("public")); 
+
+app.listen(3000, () => 
+    console.log("Servidor corriendo")
+);
+//HOME
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/index.html"))    
+});
+//PRODUCT
+app.get("/product", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/product.html"))    
+});
+//CARRITO
+   
+
+//LOGIN
+
+
+//REGISTRO
+
+
