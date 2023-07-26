2bbc128a614669e8c0c8e7fd1a2cdb9b633343f1
danielrojas76 <cdrojas.2018@gmail.com>
2023-07-26 00:18:03 -0300

    modificaciones MVC

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
 const express = require("express")
-const app = express();
-let path = require('path')
+const path = require('path')
+const mainRouter = require('./routes/mainRoutes')
+const userRouter = require('./routes/userRoutes')
+const productRouter = require('./routes/productRoutes')
 
-let mainRouter = require('./routes/mainRoutes')
-let userRouter = require('./routes/userRoutes')
-let productRouter = require('./routes/productRoutes')
+const app = express();
 
 app.use(express.static("public")); 
 
-app.set('view engine', 'ejs')
-app.set('views', [
-    path.join(__dirname, './views')
-])
+app.set('view engine', 'ejs');
 
-app.listen(3000, () => 
-    console.log("Servidor corriendo")
-);
+app.set('views', [
+    path.join(__dirname, './views'),
+    path.join(__dirname, './views/partials'),
+    path.join(__dirname, './views/main'),
+    path.join(__dirname, './views/products'),
+    path.join(__dirname, './views/users')
+]);
+
 
 app.use("/", mainRouter);
 
 app.use("/user", userRouter);
 
 app.use("/product", productRouter);
+
+app.listen(3000, () => 
+    console.log("Servidor corriendo")
+);
