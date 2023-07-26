c2a4f1226010f204ae4316a32a5620160431b33a
maticevallos <matiascevallos7@gmail.com>
2023-07-25 23:41:06 -0300

    instalen dotenv para correr el puerto

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express();
+const dotenv = require('dotenv').config();
 let path = require('path')
 
 let mainRouter = require('./routes/mainRoutes')
@@ -13,12 +14,14 @@ app.set('views', [
     path.join(__dirname, './views')
 ])
 
-app.listen(3000, () => 
-    console.log("Servidor corriendo")
-);
 
 app.use("/", mainRouter);
 
 app.use("/user", userRouter);
 
 app.use("/product", productRouter);
+
+app.listen(process.env.PORT, () => { 
+    console.log("Servidor escuchando en Puerto" + process.env.PORT);
+});
+ 
