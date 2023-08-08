76c6b7387c3407e39562f7a59b59232e87009991
danielrojas76 <cdrojas.2018@gmail.com>
2023-08-08 14:31:51 -0300

    feat: Updated entry point

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
+// ************ Require's ************
 const express = require("express");
-const dotenv = require('dotenv').config();
 const path = require('path');
-const userRouter = require('./routes/userRoutes');
-const productRouter = require('./routes/productRoutes');
-const mainRouter = require('./routes/mainRoutes');
+const dotenv = require('dotenv').config();
+const methodOverride = require('method-override');// Pasar poder usar los métodos PUT y DELETE
 
+// ************ express() - (don't touch) ************
 const app = express();
 
-app.use(express.static("public")); 
+// ************ Middlewares - (don't touch) ************
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json()); 
+app.use(methodOverride('_method')); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
 
+// ************ Template Engine - (don't touch) ************
 app.set('view engine', 'ejs');
-
 app.set('views', [
     path.join(__dirname, './views'),
     path.join(__dirname, './views/partials'),
@@ -19,14 +23,15 @@ app.set('views', [
     path.join(__dirname, './views/users')
 ]);
 
+// ************ Route System require and use() ************
+const mainRouter = require('./routes/mainRoutes');
+const userRouter = require('./routes/userRoutes');
+const productRouter = require('./routes/productRoutes');
 
 app.use("/", mainRouter);
-
 app.use("/user", userRouter);
-
 app.use("/product", productRouter);
 
 app.listen(process.env.PORT, () => { 
     console.log("Servidor escuchando en Puerto" + process.env.PORT);
 });
- 
