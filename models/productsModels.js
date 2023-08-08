310c8e5aa5080a9f069bf560f3880a52843f75e2
danielrojas76 <cdrojas.2018@gmail.com>
2023-08-08 16:54:52 -0300

    feat: crear crud detail product

--- models/productsModels.js
+++ models/productsModels.js
@@ -1,3 +1,19 @@
 const path = require('path');
 const fs = require('fs');
 
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+
+const model = {
+    findAll: () => {
+        const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+        return products;
+    },
+
+    findById: (id) =>{
+        const products = model.findAll();
+        const productFind = products.find(products => products.id === id)
+        return productFind
+    }
+};
+
+module.exports = model;
