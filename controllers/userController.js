bf5b817d613736a6c77daa46ad72006e4dc80e8a
PabloKr <pablokruchowski@gmail.com>
2023-08-28 19:40:39 -0300

    registro de usuario sprint 5

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,41 @@
+const userModels = require('../models/userModels');
+
 let userControllers = {
-    register: function(req, res){
-        res.render('register')},
 
-    login: function(req, res){
-        res.render('login')    
+    getRegister: (req, res) => {
+        const error = req.query.error;
+
+        res.render('register', { error })
     },
 
-    password: function(req, res){
-        res.render('password')    
+    register: function (req, res) {
+        const newUser = {
+            email: req.body.email,
+            password: req.body.password,
+        }
+
+        const user = userModel.create(newUser);
+
+        if (user.error) {
+            res.redirect('/users/register?error=' + user.error);
+        } else {
+            res.redirect('/');
+        }
+
     },
 
-    user: function(req, res){
+    login: function (req, res) {
+        res.render('login')
+    },
+
+    password: function (req, res) {
+        res.render('password')
+    },
+
+    user: function (req, res) {
         res.render('user')
     },
-    admin: function(req, res){
+    admin: function (req, res) {
         res.render('userAdmin')
     }
 }
